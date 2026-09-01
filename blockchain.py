bea31aa89b971d65ab629d83cd1a7868c5e67696
joseph.a.prada <josephprada.fa@academia.umb.edu.co>
2026-09-01 14:56:49 -0500

    Implement basic Blockchain class with validation

--- blockchain.py
+++ blockchain.py
@@ -1,1 +1,33 @@
+from block import Block
 
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self):
+        return Block(0, ["Bloque Génesis"], "0")
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, transactions):
+        previous_block = self.get_latest_block()
+        new_block = Block(
+            index=previous_block.index + 1,
+            transactions=transactions,
+            previous_hash=previous_block.hash
+        )
+        self.chain.append(new_block)
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i - 1]
+
+            if current_block.hash != current_block.calculate_hash():
+                return False
+
+            if current_block.previous_hash != previous_block.hash:
+                return False
+                
+        return True
