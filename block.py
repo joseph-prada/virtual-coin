c877b963b77436bdf0a9ee4b1f21d3d77eca3255
joseph.a.prada <josephprada.fa@academia.umb.edu.co>
2026-09-01 14:56:31 -0500

    Implement Block class with hashing functionality

---
+++ block.py
@@ -0,0 +1,23 @@
+import hashlib
+import json
+import time
+
+class Block:
+    def __init__(self, index, transactions, previous_hash, nonce=0):
+        self.index = index
+        self.timestamp = time.time()
+        self.transactions = transactions
+        self.previous_hash = previous_hash
+        self.nonce = nonce
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_data = {
+            "index": self.index,
+            "timestamp": self.timestamp,
+            "transactions": self.transactions,
+            "previous_hash": self.previous_hash,
+            "nonce": self.nonce
+        }
+        encoded_block = json.dumps(block_data, sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
